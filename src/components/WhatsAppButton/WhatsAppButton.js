016d3dfc0a4aa8dee1e598defee4835cb7066340
RiteshRana25 <160099737+RiteshRana25@users.noreply.github.com>
2026-01-09 18:51:29 +0530

    Update WhatsAppButton.js

--- src/components/WhatsAppButton/WhatsAppButton.js
+++ src/components/WhatsAppButton/WhatsAppButton.js
@@ -1,8 +1,8 @@
 import { FaWhatsapp } from "react-icons/fa";
-import "./WhatsAppButton.css"; // Create this CSS file or inline style
+import "./WhatsAppButton.css"; 
 
 const WhatsappButton = () => {
-  const phoneNumber = "971524460569"; // Without "+" or leading 0
+  const phoneNumber = "971524460569"; 
 
   return (
     <a
